dd0bbe57d35b5f701f01ffc55665a9393831e6a8
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-12 02:07:46 -0400

    move things around

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import SignInPage from './components/SignInPage/SignInPage';
-import SignUpPage from './components/SignUpPage/SignUpPage';
+import SignInPage from "./pages/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element = {<Index/>} />
-          <Route path="/signIn" element = {<SignInPage/>} />
-          <Route path="/signUp" element = {<SignUpPage/>} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signIn" element={<SignInPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-function Index() {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  );
-}
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,3 @@
+export default function LandingPage() {
+  return <header className="App-header">hi i'm a landing page</header>;
+}
