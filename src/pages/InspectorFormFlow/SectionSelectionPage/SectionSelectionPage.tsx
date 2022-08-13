ac48c8d04c18d288fc9a24aeb8821405e131fcf2
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-13 05:02:54 -0400

    add sections to the page

--- src/pages/InspectorFormFlow/SectionSelectionPage/SectionSelectionPage.tsx
+++ src/pages/InspectorFormFlow/SectionSelectionPage/SectionSelectionPage.tsx
@@ -1,28 +1,72 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { inspect } from "util";
+import {
+  InspectorFormSection,
+  inspectorFormSections,
+  SectionStatus,
+} from "./consts";
 import { InspectorFormContext } from "../../../contexts/InspectorFormContext";
+import "./SectionSelectionPage.css";
 
-interface SectionProps {
-  title: string;
-  handleClick?: () => void;
-}
+const Section = ({
+  collapseId,
+  headingId,
+  status,
+  title,
+  description,
+}: InspectorFormSection) => {
+  const sectionStatus =
+    status === SectionStatus.NOT_STARTED ? (
+      <span className="text-danger ps-1">(not started)</span>
+    ) : (
+      <span className="text-success">(saved)</span>
+    );
 
-const Section = ({ title, handleClick }: SectionProps) => (
-  <div className="col-sm-6 pb-2">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a onClick={handleClick} className="btn btn-primary">
-          Go somewhere
-        </a>
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id={headingId}>
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#${collapseId}`}
+          aria-expanded="false"
+          aria-controls={collapseId}
+          style={{ height: "100px" }}
+        >
+          {title} {sectionStatus}
+        </button>
+      </h2>
+      <div
+        id={collapseId}
+        className="accordion-collapse collapse"
+        aria-labelledby={headingId}
+        data-bs-parent="#accordionFlushExample"
+        style={{ height: "100px" }}
+      >
+        <div className="accordion-body">
+          {description}
+          <p />
+          {status === SectionStatus.NOT_STARTED ? (
+            <button
+              style={{ borderRadius: "8px" }}
+              className="btn p-2 w-25 btn-dark bg-dark"
+            >
+              Start
+            </button>
+          ) : (
+            <button
+              style={{ borderRadius: "8px" }}
+              className="btn p-2 w-100 btn-dark bg-dark"
+            >
+              Update
+            </button>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default function SectionSelectionPage() {
   const navigate = useNavigate();
@@ -33,27 +77,30 @@ export default function SectionSelectionPage() {
   }, [inspector]);
 
   return (
-    <div className="container pt-2">
-      <button
-        onClick={() =>
-          inspector.setForm({ ...inspector.initialForm, hi: "yo" })
-        }
-      >
-        hi
-      </button>
-      <div className="row">
-        <Section title="Basic Info" handleClick={() => navigate("/")} />
-        <Section
-          title="Red High Risk Factors"
-          handleClick={() => navigate("/")}
-        />
+    <div className="mt-5 container">
+      <div className="row px-2">
+        <div className="col-md-10">
+          <h1 className="fw-bold">Inspection Form</h1>
+          <p className="mt-3 fs-6 text-black-50">Fill in each section</p>
+        </div>
+        <div className="d-flex align-items-start col-md-2">
+          <h1 className="w-100">
+            <button
+              onClick={() => navigate("/dashboard")}
+              style={{ borderRadius: "8px" }}
+              className="btn p-2 w-100 btn-dark bg-dark"
+            >
+              Go back
+            </button>
+          </h1>
+        </div>
       </div>
-      <div className="row">
-        <Section
-          title="Blue Low Risk Factors"
-          handleClick={() => navigate("/")}
-        />
-        <Section title="Esignature" handleClick={() => navigate("/")} />
+      <div className="row px-2">
+        <div className="accordion accordion-flush" id="accordionFlushExample">
+          {inspectorFormSections.map((section, id) => (
+            <Section key={id} {...{ ...section }} />
+          ))}
+        </div>
       </div>
     </div>
   );
