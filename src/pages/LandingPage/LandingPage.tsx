b9f03998e5d04145662ab8931e62066750b829cf
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-08-20 13:44:59 -0400

    Adjusted alignment and colors on landing page. Started about page and team page.

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -62,7 +62,6 @@ export default function LandingPage() {
         </CardGroup>
       </div>
 
-
       <div className="mx-auto col-md-7">
         <p></p>
         <h2 className="d-flex justify-content-center">Features</h2>
@@ -124,72 +123,74 @@ export default function LandingPage() {
         <p></p>
       </div>
 
+      {/*FAQ*/}
       <div className="mx-auto col-md-7 d-flex justify-content-center">
-        <Accordion defaultActiveKey="0">
+        <Card bg={"primary"}>       
+          <Accordion style={{ width: '53rem' }}>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header as="p">
+                <strong>What's the difference between paperwork and online forms?</strong>
+              </Accordion.Header>
+              <Accordion.Body className="lead">
+                For paper forms, you legibly print all form data and get a client signature on-site. 
+                For online forms, you can continually update your form fields upon login. 
+                When you need a client signature, tell them to register so you can grant access to 
+                your forms signature field.
+              </Accordion.Body>
+            </Accordion.Item>
 
-        <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              What's the difference between paperwork and online forms?
-            </Accordion.Header>
-            <Accordion.Body>
-              For paper forms, you legibly print all form data and get a client signature on-site. 
-              For online forms, you can continually update your form fields upon login. 
-              When you need a client signature, tell them to register so you can grant access to 
-              your forms signature field.
-            </Accordion.Body>
-          </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header as="p">
+                <strong>How do e-signatures work?</strong>
+              </Accordion.Header>
+              <Accordion.Body className="lead">
+              An e-signature is a protected and legally binding online signature, 
+              just like a handwritten signature. Although there are many online services that offer 
+              e-signatures, we use HelloSign. As noted on their website, each signature must come from a 
+              separate account. If different names were to be signed from a single HelloSign account, 
+              it would threaten the integrity of the signatures, and could potentially invalidate a 
+              document signed in this manner. To do an e-signature, one only needs to login to their 
+              account and type their full name.
+              </Accordion.Body>
+            </Accordion.Item>
 
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>
-              How do e-signatures work?
-            </Accordion.Header>
-            <Accordion.Body>
-            An e-signature is a protected and legally binding online signature, 
-            just like a handwritten signature. Although there are many online services that offer 
-            e-signatures, we use HelloSign. As noted on their website, each signature must come from a 
-            separate account. If different names were to be signed from a single HelloSign account, 
-            it would threaten the integrity of the signatures, and could potentially invalidate a 
-            document signed in this manner. To do an e-signature, one only needs to login to their 
-            account and type their full name.
-            </Accordion.Body>
-          </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header as ="p">
+                <strong>What if your system encounters an error and I lose all my data? Can you recover it?
+                Are you liable?</strong>
+              </Accordion.Header>
+              <Accordion.Body className="lead">
+                We store all your data in a database on the cloud. In the very unlikely scenario that our 
+                database is compromised, we regularly create backups to ensure you still have your data. 
+                We are responsible for your data and are liable for any setbacks. Regardless, feel free 
+                to download PDF versions of your forms locally on your computer or even print them out. 
+                Stay in control of all your data.
+              </Accordion.Body>
+            </Accordion.Item>
 
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>
-              What if your system encounters an error and I lose all my data? Can you recover it?
-              Are you liable?
-            </Accordion.Header>
-            <Accordion.Body>
-              We store all your data in a database on the cloud. In the very unlikely scenario that our 
-              database is compromised, we regularly create backups to ensure you still have your data. 
-              We are responsible for your data and are liable for any setbacks. Regardless, feel free 
-              to download PDF versions of your forms locally on your computer or even print them out. 
-              Stay in control of all your data.
-            </Accordion.Body>
-          </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header as="p">
+                <strong>I'm both an inspector and a restaurant owner. Can I do a self inspection through the 
+                online form?</strong>
+              </Accordion.Header>
+              <Accordion.Body className="lead">
+                Yes, just sign-off on both the inspector and client signature fields.
+              </Accordion.Body>
+            </Accordion.Item>
 
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>
-              I'm both an inspector and a restaurant owner. Can I do a self inspection through the 
-              online form?
-            </Accordion.Header>
-            <Accordion.Body>
-              Yes, just sign-off on both the inspector and client signature fields.
-            </Accordion.Body>
-          </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header as="p">
+                <strong>I'm not tech-savvy. Should I still give this a shot?</strong>
+              </Accordion.Header>
+              <Accordion.Body className="lead">
+                Upon successful registration, you're all set! Build your workspace and fill out your forms. 
+                Inspectinator is desgined to be very user friendly. If you do encouter any problems or 
+                issues, contact us and we'll be sure to get back to you as soon as possible.
+              </Accordion.Body>
+            </Accordion.Item>
 
-          <Accordion.Item eventKey="4">
-            <Accordion.Header>
-              I'm not at all tech-savvy. Should I still give this a shot?
-            </Accordion.Header>
-            <Accordion.Body>
-              Upon successful registration, you're all set! Build your workspace and fill out your forms. 
-              Inspectinator is desgined to be very user friendly. If you do encouter any problems or 
-              issues, contact us and we'll be sure to get back to you as soon as possible.
-            </Accordion.Body>
-          </Accordion.Item>
-
-        </Accordion>
+          </Accordion>
+        </Card>
 
       </div>
   </div>
---
+++ src/pages/TeamPage/TeamPage.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
+export default function LandingPage() {
+  const navigate = useNavigate();
+
+  return (
+    <header className="App-header">
+      <p>hey</p>
+    </header>
+  );
+}
