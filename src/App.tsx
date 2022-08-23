f1cc9c2a0616820fe4de00c5eed5548b2648edad
Nishanth Prajith <nishanth.prajith@gmail.com>
2022-08-23 14:37:29 -0400

    Added user authentication (sign in, sign out). In addition, added an auth context which helps with storing user data for global use.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,27 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import TeamPage from "./pages/TeamPage/TeamPage";
-import SectionSelectionPage from "./pages/InspectorFormFlow/SectionSelectionPage/SectionSelectionPage";
-import AnswerSelectionPage from "./pages/InspectorFormFlow/AnswerSelectionPage/AnswerSelectionPage";
-import EsignaturePage from "./pages/InspectorFormFlow/EsignaturePage/EsignaturePage";
-import Dashboard from "./pages/Dashboard/mainDashboard/Dashboard";
+
 import InspectorFormProvider from "./contexts/InspectorFormContext";
 
 import Navbar from "./recycleComps/NavBarComp/NavBar";
+import MainCompenent from "./MainComponent";
 import Footer from "./recycleComps/footerComps/Footer";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
+  
   return (
     <div style={{minHeight: "100vh", display: "flex", flexDirection: "column"}}>
-      <InspectorFormProvider>
-        <Router>
-          <Navbar />
-          <div style={{flexGrow: '1', display: "flex",alignItems: "center", justifyContent: "center"}}>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/team" element={<TeamPage />} />
-              <Route path="/signIn" element={<SignInPage />} />
-              <Route path="/signUp" element={<SignUpPage />} />
-              <Route path="/sectionSelection" element={<SectionSelectionPage />} />
-              <Route path="/answerSelection" element={<AnswerSelectionPage />} />
-              <Route path="/eSign" element={<EsignaturePage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </div>
-          <Footer />
-        </Router>
-      </InspectorFormProvider>
+      <AuthContextProvider>
+        <InspectorFormProvider>
+          <Router>
+            <Navbar />
+            <MainCompenent />
+            <Footer />
+          </Router>
+        </InspectorFormProvider>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/pages/Dashboard/mainDashboard/Dashboard.tsx
+++ src/pages/Dashboard/mainDashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import DocumentComp from "../documentComps/DocumentComp";
 
 import { DocumentStatus } from "../documentComps/DocumentComp";
+import { useRef, useState } from "react";
 
 function run(a: number) {
   if (a % 3 === 0) {
@@ -14,75 +15,125 @@ function run(a: number) {
   } else {
     return DocumentStatus.Declined;
   }
-  
 }
 
 export default function Dashboard() {
+
+  var documents = [
+    {
+      "name" : "Inspection_report_name",
+      "restaurant" : "Dominos"
+    },
+    {
+      "name" : "Random_name",
+      "restaurant" : "Pizza hut"
+    },
+    {
+      "name" : "Recipt_from_yesterday",
+      "restaurant" : "McDonalds"
+    },
+    {
+      "name" : "Times Square Inspection",
+      "restaurant" : "Burger King"
+    }
+  ];
+
+  const [filteredDocuments, setFilteredDocuments] = useState(documents);
+
+  const searchRef = useRef<HTMLInputElement>(null);
+
+
+  function searchClick() {
+    var temp = searchRef.current?.value;
+    if (temp === undefined) {
+      temp = "";
+    }
+
+
+    if (temp === null || temp === "") {
+      setFilteredDocuments(documents);
+    } else {
+      var filteredData = [];
+      for (let i = 0; i < documents.length; i++) {
+        if (documents[i]['name'].includes(temp) || documents[i]['restaurant'].includes(temp)) {
+          filteredData.push(documents[i])
+        }
+      }
+      console.log(filteredData);
+      setFilteredDocuments(filteredData);
+    }
+  }
+
+
+
   const navigate = useNavigate();
   return (
-    <div className="container" style={{marginTop: "9vh", marginBottom: "15vh"}}>
-      <div className="row px-2">
-        <div className="col-lg-7">
-          <h1 className="fw-bold">Reports</h1>
-          <p className="mt-3 fs-6 text-black-50">
-            All document relating to your inspections are stored here.
-          </p>
-        </div>
-        <div className="col-lg-2">
-          <h1 className="p-0 mt-3 row">
-            <p className="fs-5 mb-2 col-lg-2">
-              <RiSearch2Line />
-            </p>
-            <input
-              style={{ fontSize: "0.95rem" }}
-              type="text"
-              className="text-secondary simpleSearchbar col-lg-10 y-2 px-2"
-              placeholder=""
-            ></input>
-          </h1>
-          <div style={{ height: "1px" }} className="divider bg-secondary"></div>
-        </div>
-        <div className="col-lg-1"></div>
-        <div className="d-flex align-items-start col-lg-2">
-          <h1 className="w-100">
-            <button
-              onClick={() => navigate("/sectionSelection")}
-              style={{ borderRadius: "8px" }}
-              className="btn p-2 w-100 btn-dark bg-dark"
-            >
-              Create Document
-            </button>
-          </h1>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="mt-5 pt-3 h-50">
+    <div className="container-fluid vh-80">
+      <div className="container" style={{marginTop: "9vh", marginBottom: "15vh"}}>
         <div className="row px-2">
-          <div className="col-lg-8 text-uppercase">
-            <p className="text-secondary">
-              <small>inpsection report name</small>
+          <div className="col-lg-7">
+            <h1 className="fw-bold">Reports</h1>
+            <p className="mt-3 fs-6 text-black-50">
+              All document relating to your inspections are stored here.
             </p>
           </div>
-          <div className="col-lg-2 text-uppercase">
-            <p className="text-secondary">
-              <small>created by</small>
-            </p>
+          <div className="col-lg-2">
+            <h1 className="p-0 mt-3 row">
+              <p className="fs-5 mb-2 col-lg-2">
+                <RiSearch2Line />
+              </p>
+              <input
+                style={{ fontSize: "0.95rem" }}
+                type="search"
+                className="text-secondary simpleSearchbar col-lg-10 y-2 px-2"
+                placeholder=""
+                onKeyUp={searchClick}
+                ref= {searchRef}
+                data-search
+              ></input>
+            </h1>
+            <div style={{ height: "1px" }} className="divider bg-secondary"></div>
           </div>
-          <div className="col-lg-2 text-uppercase">
-            <p className="text-secondary">
-              <small>status</small>
-            </p>
+          <div className="col-lg-1"></div>
+          <div className="d-flex align-items-start col-lg-2">
+            <h1 className="w-100">
+              <button
+                onClick={() => navigate("/sectionSelection")}
+                style={{ borderRadius: "8px" }}
+                className="btn p-2 w-100 btn-dark bg-dark"
+              >
+                Create Document
+              </button>
+            </h1>
           </div>
         </div>
-        <div style={{ height: "1px" }} className="bg-secondary mt-3 mx-2"></div>
-        <div className="mt-5">
-          {Array(12)
-            .fill(0)
-            .map((value, index) => (
-              <DocumentComp filename="Inspection_report_nam" restaurantName="Dominos Pizza, Times Square" 
-                createdDate="12-01-2022" documentstatus={run(index)}/>  
-            )
-          )}
+        <div className="divider"></div>
+        <div className="mt-5 pt-3 h-50">
+          <div className="row px-2">
+            <div className="col-lg-8 text-uppercase">
+              <p className="text-secondary">
+                <small>inpsection report name</small>
+              </p>
+            </div>
+            <div className="col-lg-2 text-uppercase">
+              <p className="text-secondary">
+                <small>created by</small>
+              </p>
+            </div>
+            <div className="col-lg-2 text-uppercase">
+              <p className="text-secondary">
+                <small>status</small>
+              </p>
+            </div>
+          </div>
+          <div style={{ height: "1px" }} className="bg-secondary mt-3 mx-2"></div>
+          <div className="mt-5">
+            {filteredDocuments.map((value, index) => (
+                <DocumentComp filename={value.name} restaurantName={value.restaurant}
+                  createdDate="12-01-2022" documentstatus={run(index)}/>  
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
