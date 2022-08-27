034fb075aa17dfb679bb1b4ee5b41b8c72a9dee5
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-27 14:55:37 -0400

    add several more questions

--- src/pages/InspectorFormFlow/AnswerSelectionPage/AnswerSelectionPage.tsx
+++ src/pages/InspectorFormFlow/AnswerSelectionPage/AnswerSelectionPage.tsx
@@ -1,3 +1,37 @@
+import { url } from "inspector";
+import { useContext, useEffect } from "react";
+import {
+  highRiskQuestions,
+  lowRiskQuestions,
+  UrlsForForm,
+} from "../../../constants/constants";
+import { InspectorFormContext } from "../../../contexts/InspectorFormContext";
+
 export default function AnswerSelectionPage() {
+  const { initialForm, setForm } = useContext(InspectorFormContext);
+  console.log(initialForm);
+
+  useEffect(() => {
+    if (window.location.pathname === UrlsForForm.lowRiskSection) {
+      setForm({
+        ...initialForm,
+        lowRisk: {
+          miniSections: [...lowRiskQuestions],
+        },
+      });
+    }
+
+    if (window.location.pathname === UrlsForForm.highRiskSection) {
+      setForm({
+        ...initialForm,
+        highRisk: {
+          miniSections: [...highRiskQuestions],
+        },
+      });
+    }
+
+    console.log(initialForm);
+  }, []);
+
   return <header className="App-header">stuff</header>;
 }
