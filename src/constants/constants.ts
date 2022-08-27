d5077f39297e8f03e9a5e05dc7727698f9e9c72e
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-27 14:41:18 -0400

    set context based on URL we are on

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,27 @@ export const UrlsForForm = {
   lowRiskSection: "/low-risk",
   highRiskSection: "/high-risk",
 };
+
+export const highRiskQuestions = [
+  {
+    section: "Demonstration of Knowledge",
+    rows: [
+      {
+        number: 1,
+        title:
+          "PIC certified by accredited program, or5 compliance with Code, or correct answers",
+        correctedDuringInspection: false,
+        repeatViolation: false,
+        pts: 5,
+      },
+      {
+        number: 2,
+        title:
+          "Food Worker Cards current for all food workers;5 new food workers trained",
+        correctedDuringInspection: false,
+        repeatViolation: false,
+        pts: 5,
+      },
+    ],
+  },
+];
