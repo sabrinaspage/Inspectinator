8055ed0b3870fc458cd215ee44db8c8615c31180
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-28 09:50:55 -0400

    reallocate
    we want each component to useContext rather than the top level page
    
    we can update the form with the relevant answers better now and make code eligible
    
    we also useState when a user is on a page, makes changes, but doesn't want to save it

---
+++ src/pages/InspectorFormFlow/AnswerSelectionPage/BasicInfoSection.tsx
@@ -0,0 +1,15 @@
+export interface BasicInfoProps {
+  section: {
+    businessName: string;
+    operator: string;
+    address: string;
+    city: string;
+    zipCode: string;
+    email: string;
+    phone: string;
+  };
+}
+
+export const BasicInfoSection = () => {
+  return <></>;
+};
