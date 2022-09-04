6f9dc46007b7477ee0c38a9dd2b30d72e7a2eb6d
MichaelSalSail <msalamo000@citymail.cuny.edu>
2022-09-04 18:38:13 -0400

    Created Embedded HelloSign API Demo App. Two routes. Shows form upon button on-click.

--- HelloSign_API_demo/hellosign.js
+++ HelloSign_API_demo/hellosign.js
@@ -1,21 +1,23 @@
+require('dotenv').config({path: "./hellosign.env"});
 const hellosign = require("hellosign-sdk")({ key: process.env.HELLOSIGN_API_KEY });
 
-const getEmbedURL = async (github_email,github_username)=>{
+
+const getEmbedURL = async (inspector_email,inspector_name)=>{
   let options = {
     test_mode: 1,
     clientId: process.env.HELLOSIGN_CLIENT_ID,
-    template_id: process.env.CLA_TEMPLATE_ID,
-    subject: 'Contributor License Agreement',
+    template_id: process.env.TEMPLATE_ID,
+    subject: 'Inspector Form',
     signers: [
       {
-        email_address: github_email,
-        name: 'Contributor',
+        email_address: inspector_email,
+        name: 'Inspector',
         role: process.env.SIGNER_ROLE_NAME
       }
     ],
     custom_fields: {  //Merge fields defined in the template
-      github_username: github_username,
-      github_email: github_email
+      inspector_name: inspector_name,
+      inspector_email: inspector_email
     }
   }
 
